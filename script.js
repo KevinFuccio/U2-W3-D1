3be00e33e7d69e7db546f585165408e4904e4bfb
kevin fuccio <kevin.fuccio00@gmai.com>
2023-01-23 15:01:08 +0100

    fixed input

--- script.js
+++ script.js
@@ -1,56 +1,34 @@
-// const oggettiRandomici ={
-//     nome: "michele",
-//     cognome : "iapichello",
-//     anni: 2050,
-//     professione: "rincoglionito",
-//     hobby: ["scroccare","mangiare","giocare","gay"]
-// }
-// const oggettiRandom2 = {...oggettiRandomici};
-// oggettiRandom2.nome = "alfio"
-// console.log(oggettiRandomici);
-// console.log(oggettiRandom2);
+let nomeInput = document.getElementById("nome");
+let cognomeInput = document.getElementById("cognome");
+let dataDiNascita = document.getElementById("data");
+let loginButton = document.getElementById("loginButton");
+let block1 = document.getElementById("block1");
+let block2 = document.getElementById("block2");
 
-// // costruttore 
-// const Person = function(){
-//     this.name = '';
-//     this.surname = '';
-//     this.age = '';
-//     this.address = '';
-//     this.showName = function () {};
-// }
-// // costruttore con parametri
-// const PersonParam = function(name,surname,age){
-//     this.name = name;
-//     this.surname = surname;
-//     this.age = age;
-//     this.address = '';
-//     this.showName = function () {};
-// }
+const User = function () {
+  this.nome = "";
+  this.cognome = "";
+  this.dataDiNascita = "";
+};
 
-// let giuseppeChiarenza = new PersonParam("Giuseppe","Chiarenza",22)
-// console.log(giuseppeChiarenza);
+console.log(User);
 
-let userInput = document.getElementById('username')
-let passwordInput = document.getElementById('password')
-let loginButton = document.getElementById('loginButton')
-let block1 = document.getElementById('block1')
-let block2 = document.getElementById('block2')
+let newUser = new User();
 
-const User = function(){
-    this.name = "";
-    this.privilegeLevel = -1 //usato per dare livelli di privilegi all'utente -1 non loggato 0 loggato 1 utente con privilegi per esempio
-}
-
-User.prototype.userType = null
-
-const Admin = function(){
-
-}
-
-let currentUser = new User()
-loginButton.addEventListener('click', () => {
-    if(userInput.value === 'user' && passwordInput.value === '123'){
-       currentUser.privilegeLevel = 0
-       currentUser.userType = null
-    }
-})
+loginButton.addEventListener("click", () => {
+  let div = document.createElement("div");
+  let table = `<table>
+                <tr>
+                    <th>Nome</th>
+                    <th>Cognome</th>
+                    <th>Data di nascita</th>
+                </tr>
+                <tr>
+                    <td>${newUser.nome = nomeInput.value}</td>
+                    <td>${newUser.cognome = cognomeInput.value}</td>
+                    <td>${newUser.dataDiNascita = dataDiNascita.value}</td>
+                </tr>
+                </table>`;
+  div.innerHTML = table;
+  document.body.appendChild(div);
+});
